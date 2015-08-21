385497496068bac32e146cd11b7d9db22cc6887f
Jevon Wright <jevon@powershop.co.nz>
2015-08-21 13:15:04 +1200

    Using FastGettext instead, and adding helper methods to emulate t()

--- collate_rails.gemspec
+++ collate_rails.gemspec
@@ -17,4 +17,6 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.rdoc"]
 
   s.add_dependency "rails", "~> 4.2.2"
+  s.add_dependency "gettext_i18n_rails"
+  s.add_dependency "gettext"
 end
--- lib/collate_rails.rb
+++ lib/collate_rails.rb
@@ -1,5 +1,19 @@
-module CollateRails
-end
+require "fast_gettext"
 
-I18n.load_path += Dir[File.dirname(__FILE__) + "/../config/locale/*.{rb,yml}"]
-puts "Collate: loaded #{I18n.available_locales} available locales"
+FastGettext.add_text_domain "app", :path => File.dirname(__FILE__) + '/../config/locale', :type => :po
+FastGettext.default_text_domain = "app"
+
+module FastGettext
+  module Translation
+    # helper method to relate to pluralize()
+    def p_(n, key)
+      n.to_s + " " + n_(key, key.pluralize, n)
+    end
+
+    # override the default i18n t() to make more sense
+    # so we don't need to do _("arg") % {args} everywhere
+    def t(string, *args)
+      _(string) % args
+    end
+  end
+end
